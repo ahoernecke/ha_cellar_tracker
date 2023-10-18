6382f07cbd2fb9d428de67fa96e7f981aa3080b6
sh00t2kill <38101689+sh00t2kill@users.noreply.github.com>
2023-10-18 17:22:10 +1100

    Add location to groups
    Add sensor for location

--- custom_components/cellar_tracker/__init__.py
+++ custom_components/cellar_tracker/__init__.py
@@ -90,7 +90,7 @@ class WineCellarData:
       df = pd.DataFrame(list)
       df[["Price","Valuation"]] = df[["Price","Valuation"]].apply(pd.to_numeric)
 
-      groups = ['Varietal', 'Country', 'Vintage', 'Producer', 'Type']
+      groups = ['Varietal', 'Country', 'Vintage', 'Producer', 'Type', 'Location']
 
       for group in groups:
         group_data = df.groupby(group).agg({'iWine':'count','Valuation':['sum','mean']})
